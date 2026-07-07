a732dffe597e5afbe0d25a643932abc6f0d2bcaa
Emanuel Teixeira <teixeira.emanuelm@gmail.com>
2026-07-06 21:12:02 -0300

    feat: CDD Design System v1.0

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { Button } from "./Button";
+export type { ButtonVariant, ButtonSize } from "./Button";
+export { Input } from "./Input";
+export { Card, CardHeader, CardTitle, CardDescription, CardFooter } from "./Card";
+export { Badge } from "./Badge";
+export { Alert } from "./Alert";
+export { Modal } from "./Modal";
+export { Table, TableHead, TableBody, TableRow, Th, Td } from "./Table";
